aa5d815ddb34c01fbb454bf731ebe66afe9fb6c6
Wolfgang Schütz <wolfgang.schuetz@anton-paar.com>
2024-05-17 15:08:42 +0200

    fixed links to toolchain in c0-toolchainfile

--- cmake_toolchain_files/arm_gcc_13.2.1__STM32C0xx.cmake
+++ cmake_toolchain_files/arm_gcc_13.2.1__STM32C0xx.cmake
@@ -1,14 +1,20 @@
 cmake_minimum_required(VERSION 3.15)
 
+if (${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
+	set(toolchain_dir      "${CMAKE_CURRENT_LIST_DIR}/../win__arm-eabi-gcc_13_2_1")
+else()
+	message(STATUS "${CMAKE_HOST_SYSTEM_NAME}")
+	message(SEND_ERROR "Host OS ${CMAKE_HOST_SYSTEM_NAME} is not supported")
+endif()
 
 set(CMAKE_SYSTEM_NAME       Generic)
 set(CMAKE_SYSTEM_VERSION    13.2.1)
 set(CMAKE_SYSTEM_PROCESSOR  STM32C0xx)
 
-set(toolchain_dir "${CMAKE_CURRENT_LIST_DIR}/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi")
-set(toolchain_bin_dir ${toolchain_dir}/bin)
-set(toolchain_inc_dir ${toolchain_dir}/arm-none-eabi/include)
-set(toolchain_lib_dir ${toolchain_dir}/arm-none-eabi/lib)
+set(TOOLS_DIR          "${CMAKE_CURRENT_LIST_DIR}/../utility")
+set(toolchain_bin_dir  "${toolchain_dir}/bin")
+set(toolchain_inc_dir  "${toolchain_dir}/arm-none-eabi/include")
+set(toolchain_lib_dir  "${toolchain_dir}/arm-none-eabi/lib")
 
 
 set(ARM_OPTIONS
@@ -49,13 +55,16 @@ set(CMAKE_C_COMPILER   "${toolchain_bin_dir}/arm-none-eabi-gcc.exe")
 set(CMAKE_CXX_COMPILER "${toolchain_bin_dir}/arm-none-eabi-g++.exe")
 set(CMAKE_ASM_COMPILER "${toolchain_bin_dir}/arm-none-eabi-gcc.exe")
 set(CMAKE_OBJCOPY      "${toolchain_bin_dir}/arm-none-eabi-objcopy.exe")
+set(CMAKE_SIZE         "${toolchain_bin_dir}/arm-none-eabi-size.exe")
 
-set(CMAKE_FIND_ROOT_PATH "${toolchain_dir}/arm-none-eabi")
+set(CMAKE_EXECUTABLE_SUFFIX ".elf")
+set(CMAKE_FIND_ROOT_PATH "${toolchain_dir}")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE     STATIC_LIBRARY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
 
 
